1311b00ff1d1e055d2ec3f5973204917a7ae0d3f
Abdo Afkir <AA@cmmco.no>
2025-08-12 15:39:46 +0200

    chore: bump SW cache to v3

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'afkir-qibla-v2';
+const CACHE = 'afkir-qibla-v3';
 const ASSETS = [
   '/', '/index.html', '/manifest.webmanifest',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/maskable-192.png', '/icons/maskable-512.png',
